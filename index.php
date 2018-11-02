a8e522a3d10f9bc004cd5e9d651bfabc992d5dbf
Bradley Ferreira <bradleyferreira@gmail.com>
2018-11-02 14:40:05 +0700

    made site cards

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Reflow Project</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1>Reflow Project</h1>
+            <div class="mySubtitle">A series of sites to help you visualize nature and the passing of time</br></div>
+            <div class="mySubtitle2">For those who are cool</div>
+            <div class="linkMenu">
+
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="assets/img/cal.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Reflow Calendar</h5>
+                        <p class="card-text">Pivot the calendar to remove extra information.</p>
+                        <a href="../reflowcalendar" class="btn btn-primary">Visit Site</a>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="assets/img/circ.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Reflow Circadian</h5>
+                        <p class="card-text">Follow the sun's patterns and catch the golden hour every day.</p>
+                        <a href="../reflowcircadian" class="btn btn-primary">Visit Site</a>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="assets/img/therm.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Reflow Thermal</h5>
+                        <p class="card-text">Convert and chart temperature relationships between F and C.</p>
+                        <a href="../reflowthermal" class="btn btn-primary">Visit Site</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
